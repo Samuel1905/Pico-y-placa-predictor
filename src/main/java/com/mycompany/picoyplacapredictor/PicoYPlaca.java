655c3f95702fd479331574e32108c0bee80621ed
Samuel <brayancunduri@outlook.es>
2025-02-05 13:00:03 -0500

    Add new method canBeOntheRoad and refactor the other methods

--- src/main/java/com/mycompany/picoyplacapredictor/PicoYPlaca.java
+++ src/main/java/com/mycompany/picoyplacapredictor/PicoYPlaca.java
@@ -1,4 +1,5 @@
 package com.mycompany.picoyplacapredictor;
+
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -9,64 +10,49 @@ import java.time.format.DateTimeFormatter;
  * @author User
  */
 public class PicoYPlaca {
+
     //atributes
     private int lastDigit;
     private LocalDate date;
     private LocalTime time;
-    
-    
+
     // Constructor 
     public PicoYPlaca(String licensePlate, String dateStr, String timeStr) {
-        this.lastDigit = Character.getNumericValue(licensePlate.charAt(licensePlate.length() - 1));
-        this.date = LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        this.time = LocalTime.parse(timeStr, DateTimeFormatter.ofPattern("HH:mm"));
-        
+        lastDigit = Character.getNumericValue(licensePlate.charAt(licensePlate.length() - 1));
+        date = LocalDate.parse(dateStr, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        time = LocalTime.parse(timeStr, DateTimeFormatter.ofPattern("HH:mm"));
     }
-    
+
     // Methods
-    // Public method to check if the vehicle can drive
-    public boolean canDrive() {
+    // Method to check if a car can be on the road
+    public boolean canBeOntheRoad() {
         return !(isRestrictedDate() && isRestrictedTime());
     }
-    
+
+    // Method to check restricted day of the week
     private boolean isRestrictedDate() {
         DayOfWeek dayofWeek = date.getDayOfWeek();
-        boolean canDrive = false;
-        
-        switch (dayofWeek) {
-            case MONDAY -> {
-                 if(lastDigit == 1 || lastDigit == 2){
-                     canDrive = true;
-                 } 
-            }
-            case TUESDAY -> {
-                if (lastDigit == 3 || lastDigit == 4) {
-                    canDrive = true;
-                }
-            }
-            case WEDNESDAY -> {
-                if (lastDigit == 5 || lastDigit == 6) {
-                    canDrive = true;
-                }
-            }
-            case THURSDAY -> {
-                if (lastDigit == 7 || lastDigit == 8) {
-                    canDrive = true;
-                }
-            }
-            case FRIDAY -> {
-                if (lastDigit == 9 || lastDigit == 0) {
-                    canDrive = true;
-                }
-            }
-            default -> canDrive= false;
-        }
-        return canDrive;
+
+        return switch (dayofWeek) {
+            case MONDAY ->
+                (lastDigit == 1 || lastDigit == 2);
+            case TUESDAY ->
+                (lastDigit == 3 || lastDigit == 4);
+            case WEDNESDAY ->
+                (lastDigit == 5 || lastDigit == 6);
+            case THURSDAY ->
+                (lastDigit == 7 || lastDigit == 8);
+            case FRIDAY ->
+                (lastDigit == 9 || lastDigit == 0);
+            default ->
+                false;
+        };
     }
-    
+
+    // Method to check restricted time
     private boolean isRestrictedTime() {
-        return (time.isAfter(LocalTime.of(6, 59)) && time.isBefore(LocalTime.of(9, 31))) ||
-               (time.isAfter(LocalTime.of(15, 59)) && time.isBefore(LocalTime.of(19, 31)));
+        return (time.isAfter(LocalTime.of(6, 59)) && time.isBefore(LocalTime.of(9, 31)))
+                || (time.isAfter(LocalTime.of(15, 59)) && time.isBefore(LocalTime.of(19, 31)));
     }
-      
+
 }
